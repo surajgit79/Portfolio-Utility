cce3f2679e36078e3c1a4f599ebb193f5c93cb3b
Piyush L. Shrestha <piyushshrestha20@gmail.com>
2026-04-08 17:14:29 +0545

    feat: Updating frontend

--- fabackend/drizzle/0000_loving_hiroim.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "users" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"email" text NOT NULL,
-	"password" text NOT NULL,
-	"role" text DEFAULT 'teacher' NOT NULL,
-	"createdAt" timestamp DEFAULT now() NOT NULL,
-	"updatedAt" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
