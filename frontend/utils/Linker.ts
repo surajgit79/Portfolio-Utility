94e632312aae60abea23e4462074c2a7bf2e1f76
surajgit79 <surajg.sci77@hsmonline.edu.np>
2026-04-08 15:05:42 +0545

    unknown commit

--- frontend/utils/Linker.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation"
-import { RedirectType } from "next/navigation"
-
-type Props = {
-    link: string
-}
-export const Linker = ({link}: Props) => {
-    redirect(link, RedirectType.replace)
-}
